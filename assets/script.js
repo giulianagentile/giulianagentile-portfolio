3f1d4cc01622a95f36902e956ac689746fd057b4
giulianagentile <95339896+giulianagentile@users.noreply.github.com>
2021-12-01 00:14:28 -0800

    Portfolio

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector(".loader-container").style.visibility = "visible";
+    } else {
+        document.querySelector(".loader-container").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+};
